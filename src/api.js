2d131909f722ec85a982ef752735bf4ea6423e64
张尧 <guilishabai@126.com>
2019-10-25 17:21:21 +0800

    goodsinfo

--- src/api.js
+++ src/api.js
@@ -6,6 +6,6 @@ module.exports = {
   login: URL + "/api/mobile/index.php?w=logout&t=index",
   // 首页—商品列表接口
   indexGoodsList: URL + "/api/mobile/index.php?t=goods_list&w=goods",
-  // 首页—获取默认数据接口
-  indexDefaultInfo: URL + "/api/mobile/index.php?w=index&t=index",
+  // 普通商品详情
+  ordinaryGoodsDtail: URL + "/api/mobile/index.php?t=goods_detail&w=goods&=36",
 }
