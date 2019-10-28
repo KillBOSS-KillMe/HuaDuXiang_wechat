a92ef4fe7ee34736c3b92acd06e53bddbc647377
张尧 <guilishabai@126.com>
2019-10-28 15:19:27 +0800

    dist

--- dist/_wepylogs.js
+++ dist/_wepylogs.js
@@ -1,2 +1,2 @@
 console.log('WePY开启错误监控');
-console.warn("CLI报错：WARNING IN : src\pages\index.wpy\n[xmldom warning]	unclosed xml attribute\n@#[line:229,col:9]");
+console.warn("CLI报错：发现空文件: C:\Users\hongyi\Desktop\HuaDuXiang_wechat\src\pages\index.wpy");
