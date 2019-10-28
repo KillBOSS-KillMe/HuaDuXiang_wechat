28be26ade888713be6849f411bc51a4fb532b3ef
张尧 <guilishabai@126.com>
2019-10-28 09:42:03 +0800

    wechat

--- src/api.js
+++ src/api.js
@@ -7,5 +7,7 @@ module.exports = {
   // 首页—商品列表接口
   indexGoodsList: URL + "/api/mobile/index.php?t=goods_list&w=goods",
   // 普通商品详情
-  ordinaryGoodsDtail: URL + "/api/mobile/index.php?t=goods_detail&w=goods&=36",
+  ordinaryGoodsDtail: URL + "/api/mobile/index.php?t=goods_detail&w=goods",
+  // 首页-轮播图和areaArr菜单
+  indexDefaultInfo: URL + "/api/mobile/index.php?w=index&t=index"
 }
