342f44f81ac5682201efc8de4a501678389a67e1
张尧 <guilishabai@126.com>
2019-11-06 17:04:37 +0800

    wechat

--- dist/_wepylogs.js
+++ dist/_wepylogs.js
@@ -1,3 +1,1 @@
 console.log('WePY开启错误监控');
-console.warn("CLI报错：WARNING IN : src\pages\index.wpy\n[xmldom warning]	unclosed xml attribute\n@#[line:235,col:9]");
-console.warn("CLI报错：WARNING IN : src\pages\index.wpy\n[xmldom warning]	unclosed xml attribute\n@#[line:235,col:9]");
