4e890fabd9951233142235e3ea13637153803ccd
张尧 <guilishabai@126.com>
2019-11-12 19:21:13 +0800

    weahct

--- dist/_wepylogs.js
+++ dist/_wepylogs.js
@@ -1,1 +1,3 @@
 console.log('WePY开启错误监控');
+console.warn("CLI报错：WARNING IN : src\pages\index.wpy\n[xmldom warning]	unclosed xml attribute\n@#[line:236,col:9]");
+console.warn("CLI报错：WARNING IN : src\components\pas_popup.wpy\n[xmldom warning]	unclosed xml attribute\n@#[line:81,col:9]");
